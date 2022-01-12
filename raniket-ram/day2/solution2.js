df6aa98a928122da96244ed1bdbd9c7a94a959a9
Raniket Ram <raniket.ram@paytmmoney.com>
2022-01-12 14:33:55 +0530

    day2 solution

--- raniket-ram/day2/solution2.js
+++ raniket-ram/day2/solution2.js
@@ -6,20 +6,32 @@
  * @return {number}
  */
 function solution(allowed, words) {
-  // Write your code here
-  
+  let counter = 0
+
+  function isValid(word, allowed) {
+    let allowedChars = allowed.split('')
+    let ans = true
+    for (let i = 0; i < word.length; i++)
+      if (allowedChars.indexOf(word.charAt(i)) < 0) ans = false
+    return ans
+  }
+
+  for (let i = 0; i < words.length; i++)
+    if (isValid(words[i], allowed)) counter++
+
+  return counter
 };
 
 
 // --------------------------------- Code Driver ---------------------------------
 
-let result1 = solution("ab", ["ad","bd","aaab","baa","badab"])
+let result1 = solution("ab", ["ad", "bd", "aaab", "baa", "badab"])
 let expected1 = 2
 
-let result2 = solution("abc", ["a","b","c","ab","ac","bc","abc"])
+let result2 = solution("abc", ["a", "b", "c", "ab", "ac", "bc", "abc"])
 let expected2 = 7
 
-let result3 = solution("cad", ["cc","acd","b","ba","bac","bad","ac","d"])
+let result3 = solution("cad", ["cc", "acd", "b", "ba", "bac", "bad", "ac", "d"])
 let expected3 = 4
 
 console.log(result1 === expected1 ? 'Test Passed' : 'Test Failed')
