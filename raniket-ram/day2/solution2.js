5b06ba30785e9a9e9f7468a6449b0e2cf7ea2112
Raniket Ram <raniket.ram@paytmmoney.com>
2022-01-12 14:31:18 +0530

    day2 problems

---
+++ raniket-ram/day2/solution2.js
@@ -0,0 +1,27 @@
+// --------------------------------- Solution 1 ---------------------------------
+
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+function solution(allowed, words) {
+  // Write your code here
+  
+};
+
+
+// --------------------------------- Code Driver ---------------------------------
+
+let result1 = solution("ab", ["ad","bd","aaab","baa","badab"])
+let expected1 = 2
+
+let result2 = solution("abc", ["a","b","c","ab","ac","bc","abc"])
+let expected2 = 7
+
+let result3 = solution("cad", ["cc","acd","b","ba","bac","bad","ac","d"])
+let expected3 = 4
+
+console.log(result1 === expected1 ? 'Test Passed' : 'Test Failed')
+console.log(result2 === expected2 ? 'Test Passed' : 'Test Failed')
+console.log(result3 === expected3 ? 'Test Passed' : 'Test Failed')
