40db7c92c82f61e919a6bc7b5fdbe63d8efd001a
Raniket Ram <raniket.ram@paytmmoney.com>
2022-01-13 18:59:39 +0530

    Added problmes for day 3

---
+++ raniket-ram/day3/solution2.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+function solution(words) {
+  // Write your code here
+};
+
+// -------------------------------- Driver -------------------------------- //
+
+let result1 = solution(["gin","zen","gig","msg"])
+let expected1 = 2
+
+let result2 = solution(["a"])
+let expected2 = 1
+
+console.log(result1 === expected1 ? 'PASSED' : 'FAILED')
+console.log(result2 === expected2 ? 'PASSED' : 'FAILED')
